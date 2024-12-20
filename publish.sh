496a181ff68037f1dd0b2fef3de28c13f0469f68
Michelangelo Matina <mikeymat12@gmail.com>
2024-12-20 20:48:37 +0100

    Updated publish.sh

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 python -m pip install --upgrade build
 python -m build
 python -m pip install --upgrade twine    
-python3 -m twine upload --repository testpypi dist/* 
+python3 -m twine upload --repository pypi dist/*
 rm -rf ./dist       
